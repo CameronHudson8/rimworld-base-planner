dbb848567b7729683e4168abc51b48cf7d370ec0
Cameron Hudson <CameronHudson8@gmail.com>
2024-04-21 16:07:22 -0700

    refactor: convert App to function component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { ReactElement } from 'react';
 import './App.css';
 import { Base } from './components/Base';
 import { RoomProps } from './components/Room';
@@ -8,224 +8,216 @@ export type AppProps = {}
 export type AppState = {
   rooms: RoomProps[];
 }
+export function App(props: AppProps): ReactElement {
 
-export class App extends React.Component<AppProps, AppState> {
-
-  constructor(props: AppProps) {
-    super(props);
-    this.state = {
-      rooms: [
+  const rooms = [
+    {
+      links: [],
+      name: "batteries",
+      size: 1
+    },
+    {
+      links: [],
+      name: "bedroom-1",
+      size: 1
+    },
+    {
+      links: [],
+      name: "bedroom-2",
+      size: 1
+    },
+    {
+      links: [],
+      name: "bedroom-3",
+      size: 1
+    },
+    {
+      links: [],
+      name: "bedroom-4",
+      size: 1
+    },
+    {
+      links: [],
+      name: "bedroom-5",
+      size: 1
+    },
+    {
+      links: [],
+      name: "bedroom-6",
+      size: 1
+    },
+    {
+      links: [],
+      name: "bedroom-7",
+      size: 1
+    },
+    {
+      links: [],
+      name: "bedroom-8",
+      size: 1
+    },
+    {
+      links: [],
+      name: "bedroom-9",
+      size: 1
+    },
+    {
+      links: [],
+      name: "bedroom-10",
+      size: 1
+    },
+    {
+      links: [],
+      name: "bedroom-11",
+      size: 1
+    },
+    {
+      links: [],
+      name: "bedroom-12",
+      size: 1
+    },
+    {
+      links: [
         {
-          links: [],
-          name: "batteries",
-          size: 1
+          name: "pen-general"
         },
         {
-          links: [],
-          name: "bedroom-1",
-          size: 1
+          name: "storage-refrigerated"
         },
         {
-          links: [],
-          name: "bedroom-2",
-          size: 1
-        },
-        {
-          links: [],
-          name: "bedroom-3",
-          size: 1
-        },
-        {
-          links: [],
-          name: "bedroom-4",
-          size: 1
-        },
-        {
-          links: [],
-          name: "bedroom-5",
-          size: 1
-        },
-        {
-          links: [],
-          name: "bedroom-6",
-          size: 1
-        },
-        {
-          links: [],
-          name: "bedroom-7",
-          size: 1
-        },
-        {
-          links: [],
-          name: "bedroom-8",
-          size: 1
-        },
-        {
-          links: [],
-          name: "bedroom-9",
-          size: 1
-        },
-        {
-          links: [],
-          name: "bedroom-10",
-          size: 1
-        },
-        {
-          links: [],
-          name: "bedroom-11",
-          size: 1
-        },
-        {
-          links: [],
-          name: "bedroom-12",
-          size: 1
-        },
-        {
-          links: [
-            {
-              name: "pen-general"
-            },
-            {
-              name: "storage-refrigerated"
-            },
-            {
-              name: "storage-unrefrigerated"
-            }
-          ],
-          name: "butcher",
-          size: 1
-        },
-        {
-          links: [
-            {
-              name: "storage-refrigerated"
-            }
-          ],
-          name: "hospital",
-          size: 1
-        },
-        {
-          links: [
-            {
-              name: "storage-refrigerated"
-            },
-            {
-              name: "storage-unrefrigerated"
-            }
-          ],
-          name: "kitchen",
-          size: 1
-        },
-        {
-          links: [
-            {
-              name: "butcher"
-            },
-            {
-              name: "pen-male-chickens"
-            },
-            {
-              name: "storage-refrigerated"
-            }
-          ],
-          name: "pen-general",
-          size: 1
-        },
-        {
-          links: [
-            {
-              name: "pen-general"
-            },
-            {
-              name: "storage-refrigerated"
-            }
-          ],
-          name: "pen-male-chickens",
-          size: 1
-        },
-        {
-          links: [
-            {
-              name: "storage-refrigerated"
-            }
-          ],
-          name: "prison",
-          size: 1
-        },
-        {
-          links: [
-            {
-              name: "storage-refrigerated"
-            }
-          ],
-          name: "rec-room-and-temple",
-          size: 6
-        },
-        {
-          links: [
-            {
-              name: "butcher"
-            },
-            {
-              name: "hospital"
-            },
-            {
-              name: "kitchen"
-            },
-            {
-              name: "pen-general"
-            },
-            {
-              name: "pen-male-chickens"
-            },
-            {
-              name: "prison"
-            },
-            {
-              name: "rec-room-and-temple"
-            }
-          ],
-          name: "storage-refrigerated",
-          size: 5
-        },
-        {
-          links: [
-            {
-              name: "butcher"
-            },
-            {
-              name: "kitchen"
-            },
-            {
-              name: "workshop"
-            }
-          ],
-          name: "storage-unrefrigerated",
-          size: 5
-        },
-        {
-          links: [
-            {
-              name: "storage-unrefrigerated"
-            }
-          ],
-          name: "workshop",
-          size: 5
+          name: "storage-unrefrigerated"
         }
       ],
-    };
-  }
+      name: "butcher",
+      size: 1
+    },
+    {
+      links: [
+        {
+          name: "storage-refrigerated"
+        }
+      ],
+      name: "hospital",
+      size: 1
+    },
+    {
+      links: [
+        {
+          name: "storage-refrigerated"
+        },
+        {
+          name: "storage-unrefrigerated"
+        }
+      ],
+      name: "kitchen",
+      size: 1
+    },
+    {
+      links: [
+        {
+          name: "butcher"
+        },
+        {
+          name: "pen-male-chickens"
+        },
+        {
+          name: "storage-refrigerated"
+        }
+      ],
+      name: "pen-general",
+      size: 1
+    },
+    {
+      links: [
+        {
+          name: "pen-general"
+        },
+        {
+          name: "storage-refrigerated"
+        }
+      ],
+      name: "pen-male-chickens",
+      size: 1
+    },
+    {
+      links: [
+        {
+          name: "storage-refrigerated"
+        }
+      ],
+      name: "prison",
+      size: 1
+    },
+    {
+      links: [
+        {
+          name: "storage-refrigerated"
+        }
+      ],
+      name: "rec-room-and-temple",
+      size: 6
+    },
+    {
+      links: [
+        {
+          name: "butcher"
+        },
+        {
+          name: "hospital"
+        },
+        {
+          name: "kitchen"
+        },
+        {
+          name: "pen-general"
+        },
+        {
+          name: "pen-male-chickens"
+        },
+        {
+          name: "prison"
+        },
+        {
+          name: "rec-room-and-temple"
+        }
+      ],
+      name: "storage-refrigerated",
+      size: 5
+    },
+    {
+      links: [
+        {
+          name: "butcher"
+        },
+        {
+          name: "kitchen"
+        },
+        {
+          name: "workshop"
+        }
+      ],
+      name: "storage-unrefrigerated",
+      size: 5
+    },
+    {
+      links: [
+        {
+          name: "storage-unrefrigerated"
+        }
+      ],
+      name: "workshop",
+      size: 5
+    }
+  ];
 
-  render() {
-    return (
-      <div
-        className="container"
-      >
-        <h1>Rimworld Base Planner</h1>
-        <Base
-          rooms={this.state.rooms}
-        ></Base>
-      </div>
-    );
-  }
+  return (
+    <div
+      className="container"
+    >
+      <h1>Rimworld Base Planner</h1>
+      <Base
+        rooms={rooms}
+      ></Base>
+    </div>
+  );
 }
