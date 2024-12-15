d6c944999791bfc7961b47721c31b4f0823ec794
Cameron Hudson <CameronHudson8@gmail.com>
2024-12-15 09:46:48 -0800

    ci: set up eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,33 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import reactPlugin from 'eslint-plugin-react';
+import tseslint from "typescript-eslint";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    ignores: ["build/*"],
+  },
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  {
+    // This is not a plugin object, but a shareable config object
+    ...reactPlugin.configs.flat.recommended,
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+  reactPlugin.configs.flat['jsx-runtime'],
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  pluginJs.configs.recommended,
+  // ...tseslint.configs.recommended,
+  ...(tseslint.configs.recommended.map((tsEslintConfig) => ({
+    ...tsEslintConfig,
+    ignores: [
+      "config/**/*",
+      "scripts/*",
+    ],
+  }))),
+];
